297e7831a223a62e0f602e367bbec3f9eef7bec9
BiteDasher <cazorax@mail.ru>
2020-05-08 19:45:44 +0300

    Change /usr/bin/bash to /bin/bash

--- debian_patch.sh
+++ debian_patch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 sed "s/PREFIX\ \:\=\ \/usr\/local/PREFIX\ \:\=\ \/usr/g" -i Makefile
 sed 's/PK_EXEC_DEST \:\= \\\/usr\\\/local\\\/bin/PK_EXEC_DEST \:\= \\\/usr\\\/bin/g' -i Makefile
 sed "s/git\ submodule\ update\ --init\ --recursive\ &\&\ cd\ wifiman-indicator/git\ submodule\ update\ --init\ --recursive\ \&\&\ cd\ wifiman-indicator\ \&\&\ .\/debian_patch.sh/g" -i Makefile
--- deps_check.sh
+++ deps_check.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 if ! [ "$(command -v iwconfig)" ]; then echo "wireless-tools not found" >> .tmp; berror=1; fi
 if ! [ "$(command -v wpa_supplicant)" ]; then echo "wpa_supplicant not found" >> .tmp; berror=1; fi
 if ! [ "$(command -v sed)" ]; then echo "sed not found" >> .tmp; berror=1; fi
