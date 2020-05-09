6d38e8d32fdba010668b68bc7885d2f6cf9d935f
BiteDasher <cazorax@mail.ru>
2020-05-09 14:00:48 +0300

    Fix some debian patch

--- debian_patch.sh
+++ debian_patch.sh
@@ -2,4 +2,5 @@
 sed "s/PREFIX\ \:\=\ \/usr\/local/PREFIX\ \:\=\ \/usr/g" -i Makefile
 sed 's/PK_EXEC_DEST \:\= \\\/usr\\\/local\\\/bin/PK_EXEC_DEST \:\= \\\/usr\\\/bin/g' -i Makefile
 sed "s/git\ submodule\ update\ --init\ --recursive\ \&\&\ cd\ wifiman-indicator/git\ submodule\ update\ --init\ --recursive\ \&\&\ cd\ wifiman-indicator\ \&\&\ .\/debian_patch.sh/g" -i Makefile
+# Because PKExec at Debian doesn't include /usr/sbin in PATH
 sed "s/\#\#\#/PATH\=\$PATH\:\/usr\/sbin\//g" -i wifiman wifigui
